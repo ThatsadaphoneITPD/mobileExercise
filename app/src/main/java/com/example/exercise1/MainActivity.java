898d29375e3b092227a0f3c9d4e1a3b7284e6b2e
thatsadaphone <thatsadaphoneigcd191062@fpt.edu.vn>
2022-10-26 12:52:33 +0700

    code pic

--- app/src/main/java/com/example/exercise1/MainActivity.java
+++ app/src/main/java/com/example/exercise1/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.exercise1;
 
 import androidx.annotation.NonNull;
@@ -30,7 +31,6 @@ import com.squareup.picasso.Picasso;
 import com.squareup.picasso.Target;
 
 import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity {
     ImageView imageView;
     ArrayList<Bitmap> imageUrls;
